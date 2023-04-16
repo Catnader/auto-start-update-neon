ac15331d4b3b1c681811130ecd22a28818125352
Isaiah Jankowski <70085043+Catnader@users.noreply.github.com>
2023-04-16 16:59:56 -0500

    Add files via upload

---
+++ autostart_update_neon.sh
@@ -0,0 +1,11 @@
+
+echo "Welcome Back!" $USER
+echo "It is: "
+date
+echo "checking for updates"
+sudo pkcon update&& sudo pkcon upgrade
+echo""
+echo""
+echo "Goodbye!"
+echo "Made by Isaiah Jankowski "
+echo " Contact Me:isaiahjankowski640@gmail.com "
